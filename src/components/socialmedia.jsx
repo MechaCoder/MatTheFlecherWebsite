ab35902ec22bc57cd1122df54a95d4d092b3a24f
Matthew Proudman <proudmandandc@gmail.com>
2021-04-06 22:24:27 +0100

    improvements to sm pannel

--- src/components/socialmedia.jsx
+++ src/components/socialmedia.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons';
+// import {} from '@fortawesome/free-regular-svg-icons';
+import {faMugHot} from '@fortawesome/free-solid-svg-icons';
 
 
 export default class SocialMedia extends React.Component {
@@ -17,8 +19,13 @@ export default class SocialMedia extends React.Component {
             </a>
           </div>
           <div className="instergram">
-          <a href="https://www.instagram.com/mat_the_fletcher/"> 
-              <FontAwesomeIcon icon={faInstagram} />
+            <a href="https://www.instagram.com/mat_the_fletcher/"> 
+                <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
+          <div className="kifo" >
+            <a href='https://ko-fi.com/matthefletcher'>
+              <FontAwesomeIcon icon={faMugHot} />
             </a>
           </div>
         </div>
